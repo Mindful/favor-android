52e574608e960b62a19175c97b7405f7c649871e
Rebar Niemi <rebar.niemi@gmail.com>
2013-05-06 11:42:23 -0700

    Standard Dev

--- src/com/favor/util/Algorithms.java
+++ src/com/favor/util/Algorithms.java
@@ -33,52 +33,84 @@ public class Algorithms {
 	  return ratio;
   }
 
-  public static double[] responseTime (String address, long fromDate, long untilDate)
-	{
-	  DataHandler db = DataHandler.get();
-	  LinkedList<textMessage> list = db.queryConversation(address, fromDate, untilDate);
-		//well, averages are obv wrong, but the consecutive stripping works like a charm
-		long sendTotal = 0;
-		long receiveTotal = 0;
-		double avgSentTime;
-		double avgRecTime;
-		double [] ratios = {0, 0};
-		ArrayList<Long> sentTimes = new ArrayList<Long>();
-		ArrayList<Long> recTimes = new ArrayList<Long>();
-		textMessage temp, prev = null;
-		Long time;
-		int tempSentCount = 0;
-		int tempRecCount = 0;
-		while(list.peekLast()!=null)
-		{
-		    temp = list.pollLast(); //removes from queue
-			if (prev!= null)
-			{
-				time = temp.rawDate() - prev.rawDate(); //make time negative, because it will be. also consider switch ifs?
-				if (temp.received()){ sentTimes.add(time); sendTotal += time; tempSentCount++; } //our response time
-				else {recTimes.add(time); receiveTotal += time; tempRecCount++;}
-				
-			}
-			while(list.peekLast() != null && list.peekLast().received() == temp.received()) //short circuits
-			{
-				if (list.peekLast().received())	Debug.log("received:"+list.peekLast().textDate()+" temp:"+temp.textDate());
-				else Debug.log("sent:"+list.peekLast().textDate()+" temp:"+temp.textDate());
-	
-				list.pollLast();
-			}
-			prev = temp;
-		}
-		Debug.log("sentTimes"+sentTimes.size()+"tempSent"+tempSentCount);
-		Debug.log("recTimes"+recTimes.size()+"tempRec"+tempRecCount);
-		avgSentTime = sendTotal/(1000*tempSentCount); //sentTimes.size();
-		avgRecTime = receiveTotal/(1000*tempRecCount); //recTimes.size();
-		ratios[0] = avgSentTime;
-		ratios[1] = avgRecTime;
-		return ratios;
-	}
+  public static long[] responseTime (String address, long fromDate, long untilDate)
+ 	{
+ 	  DataHandler db = DataHandler.get();
+ 	  LinkedList<textMessage> list = db.queryConversation(address, fromDate, untilDate);
+ 		//well, averages are obi wrong, but the consecutive stripping works like a charm
+ 		long sendTotal = 0;
+ 		long receiveTotal = 0;
+ 		long avgSentTime;
+ 		long avgRecTime;
+ 		long [] ratios = {0, 0};
+ 		ArrayList<Long> sentTimes = new ArrayList<Long>();
+ 		ArrayList<Long> recTimes = new ArrayList<Long>();
+ 		textMessage temp, prev = null;
+ 		Long time;
+ 		int tempSentCount = 0;
+ 		int tempRecCount = 0;
+ 		ArrayList<Long> checkSentTimes = new ArrayList<Long>();
+ 		ArrayList<Long> checkRecTimes = new ArrayList<Long>();
+ 		while(list.peekLast()!=null)
+ 		{
+ 		    temp = list.pollLast(); //removes from queue
+ 			if (prev!= null)
+ 			{
+ 				time = temp.rawDate() - prev.rawDate(); //make time negative, because it will be. also consider switch ifs?
+ 				if (temp.received()){ checkSentTimes.add(time); tempSentCount++; } //our response time
+ 				else {checkRecTimes.add(time); tempRecCount++;}
+ 				
+ 			}
+ 			while(list.peekLast() != null && list.peekLast().received() == temp.received()) //short circuits
+ 			{
+ 				if (list.peekLast().received())	Debug.log("received:"+list.peekLast().textDate()+" temp:"+temp.textDate());
+ 				else Debug.log("sent:"+list.peekLast().textDate()+" temp:"+temp.textDate());
+ 	
+ 				list.pollLast();
+ 			}
+ 			prev = temp;
+ 		}
+ 		int n1 = checkSentTimes.size(); //n for sent
+ 		int n2 = checkRecTimes.size(); //n for received
+ 		for (int i=0;i<n1;i++) Debug.log(" THIS IS A CHECK OF SENT TIMES" + checkSentTimes.get(i)+ " "  + i);
+ 		for (int i=0;i<n2;i++) Debug.log(checkRecTimes.get(i) + " This is a check of received times" + i);
+ 		double meanSent = 0; // Y-bar for the sent times
+ 		double meanRec = 0; // Y-bar for the received times
+ 		double sentStdDev = 0;
+ 		double recStdDev = 0;
+ 		for (int i = 0; i < n1; i++) meanSent += checkSentTimes.get(i);
+ 		for (int i = 0; i < n2; i++) meanRec += checkRecTimes.get(i);
+ 		meanSent = meanSent/n1;
+ 		meanRec = meanRec/n2;
+ 		for (int i = 0; i < n1; i++) sentStdDev += (checkSentTimes.get(i) - meanSent)*(checkSentTimes.get(i) - meanSent);
+ 		for (int i = 0; i < n2; i++) recStdDev += (checkRecTimes.get(i) - meanRec)*(checkRecTimes.get(i) - meanRec);
+ 		sentStdDev = Math.sqrt((1/(n1))*sentStdDev);
+ 		recStdDev = Math.sqrt((1/(n2))*recStdDev);
+ 		for (int i = 0; i < n1; i++) {
+ 			if (checkSentTimes.get(i) > (meanSent + (2*sentStdDev)) || checkSentTimes.get(i) < (meanSent - (2*sentStdDev)))
+ 				checkSentTimes.remove(i);
+ 		}
+ 		for (int i = 0; i < n2; i++) {
+ 			if (checkRecTimes.get(i) > (meanRec + (2*recStdDev)) || checkRecTimes.get(i) < (meanRec - (2*recStdDev)))
+ 				checkRecTimes.remove(i);
+ 		}
+ 		sentTimes = checkSentTimes;
+ 		recTimes = checkRecTimes;
+ 		for (int i=0;i<n1;i++) Debug.log(sentTimes.get(i) + " THIS IS A CHECK OF SENT TIMES after clean" + i);
+ 		for (int i=0;i<n2;i++) Debug.log(recTimes.get(i) + " This is a check of received times after clean" + i);
+ 		
+ 		Debug.log("sentTimes"+sentTimes.size()+"tempSent"+tempSentCount);
+ 		Debug.log("recTimes"+recTimes.size()+"tempRec"+tempRecCount);
+ 		avgSentTime = sendTotal/(1000*tempSentCount); //sentTimes.size();
+ 		avgRecTime = receiveTotal/(1000*tempRecCount); //recTimes.size();
+ 		ratios[0] = avgSentTime;
+ 		ratios[1] = avgRecTime;
+ 		return ratios;
+ 	}
+
   
   	public static double responseRatio (String address, long fromDate, long untilDate) {
-	  double [] times = responseTime(address, fromDate, untilDate);
+	  long[] times = responseTime(address, fromDate, untilDate);
 	  double ratio = times[1]/times[0];
 	  return ratio;
   	}
